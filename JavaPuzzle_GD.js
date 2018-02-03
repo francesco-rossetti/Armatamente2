faabc37b8024b2b6a0834834382d4aaa01550a38
Francesco Rossetti <francross59@gmail.com>
2018-02-03 19:09:20 +0100

    03022018

---
+++ JavaPuzzle_GD.js
@@ -0,0 +1,315 @@
+//Instanziare gli elementi per le domande
+//url('../images/divbg.png')
+answ1 = "Drones flying over Afghanistan or various targets in Africa are controlled from Creech Air Force Base (near Las Vegas), or a base in New Mexico, thousands of km away from the vehicles themselves. The very fact that the human controllers who release the weapons are very far away, so that they do not see the blood and destruction directly but only from the drone’s cameras means that for some of them such activities are more like a video game rather than the killing and destruction of human beings.";
+q1 = ["Sì, perché si trova a migliaia di chilometri dal teatro di guerra", "Sì, perché non vede che cosa succede alle sue vittime", "No, perché è pur sempre un soldato che combatte per difendere la propria patria", "No, perché sa bene che non si tratta di un videogioco"];
+c1 = "Sì, perché si trova a migliaia di chilometri dal teatro di guerra";
+im1 = "puzzle3/im1.jpg";
+pdf1="pdf/GUERRADIGITALE__PilotiDroni.pdf";
+
+answ2 = "Nella locandina di questo film, ambientato nella guerra in Iraq, vediamo un ordigno artigianale, ma capace di infliggere perdite anche ad un esercito tecnologicamente avanzato. Questo tipo di conflitto può essere considerato:";
+q2 = ["Una guerra asimmetrica perché una o più parti in conflitto non sono costituite da un esercito regolare ", "Una guerra simmetrica, perché gli avversari si affrontano schierando ciascuno il proprio esercito", "Una guerra simmetrica perché lo scontro avviene tra combattenti", "Una guerra asimmetrica perché l’esercito americano ha sempre la vittoria sul nemico"];
+c2 = "Una guerra asimmetrica perché una o più parti in conflitto non sono costituite da un esercito regolare ";
+im2 = "puzzle3/im2.jpg";
+pdf2="pdf/GUERRADIGITALE_The Hurt Locker.pdf";
+
+answ3 = "* Il soldato digitale non si presenta mai con le proprie generalità, ma cambia molto spesso nome sia in rete che nella vita reale "+
+"* Difficilmente o quasi mai utilizza armi vere "+
+"* La sua principale arma è un pc connesso ad internet "+
+"* Non combatte su un fronte reale, ma il web è il suo campo di battaglia"+
+"* Ha una grande capacità organizzativa nello svolgere gli attacchi e nel portarli a termine "+
+"A quale figura si riferisce la descrizione del soldato digitale che hai appena letto?";
+q3 = ["all’hacker", "al drone", "al soldato dei racconti di fantascienza", "al foreign fighter"];
+c3 = "all’hacker";
+im3 = "puzzle3/im3.jpg";
+pdf3="pdf/GUERRADIGITALE__Dal_cannone_al_mouse_cronaca_della_prima_guerra_digitale.pdf";
+
+answ4 = "La violazione della rete dell’ISIS da parte dell’associazione di hacker detta “Anonymous” presenta implicazioni psicologiche da parte di chi la commette?";
+q4 = ["Sì, dal momento che si tratta di combattere la propaganda, anche psicologica, che i jihadisti portano avanti", "Sì, perché crea un senso di superiorità nell’hacker", "No, perché Anonymous è gestito solo da computer", "No, perché è vissuta come un semplice allenamento informatico"];
+c4 = "Sì, dal momento che si tratta di combattere la propaganda, anche psicologica, che i jihadisti portano avanti";
+im4 = "puzzle3/im4.jpg";
+pdf4="pdf/GUERRADIGITALE__Anonymous_così_abbiamo_violato_la_rete_dell_ISIS.pdf";
+
+answ5 = "Nel film American Sniper, il regista vuole fare riflettere sullo stress psicologico di un particolare tipo di soldato che opera in un teatro di guerra asimmetrica: il cecchino. Quale rapporto si instaura tra il soldato e la sua arma: ";
+q5 = ["Il mirino del suo fucile diventa un prolungamento dell’occhio del soldato", "Il soldato si sente sicuro perché il suo fucile è infallibile", "Il cecchino finisce per odiare il suo fucile, perché gli impedisce di osservare il mondo con i propri occhi", "Per il cecchino moderno, il fucile è un’arma come un’altra, benché molto tecnologica"];
+c5 = "Il mirino del suo fucile diventa un prolungamento dell’occhio del soldato";
+im5 = "puzzle3/im5.jpg";
+pdf5="pdf/GUERRADIGITALE_AMERICAN_SNIPER_1_.pdf";
+
+answ6 = "Il contributo evidenzia la crisi del senso di sicurezza e libertà dei civili anche in rete, fra i servizi e gli strumenti tecnologici. Quale caratteristica emerge dei nuovi soldati?";
+q6 = ["Presenza anche nel virtuale.", "Scopo politico", "Il senso di superiorità", "La ristrettezza culturale"];
+c6 = "Presenza anche nel virtuale.";
+im6 = "puzzle3/im6.jpg";
+pdf6="pdf/GUERRADIGITALE_CHARLIEHEBDO.pdf";
+
+answ7 = "Nella locandina del film qui raffigurata si può intravvedere l’immagine di un soldato che: ";
+q7 = ["Viene condizionato ad essere una violenta macchina da guerra", "indossa un elmetto aggressivo per terrorizzare il nemico", "Nonostante tutto crede nella pace", "Vive la guerra come un’esperienza drammatica"];
+c7 = "Viene condizionato ad essere una violenta macchina da guerra";
+im7 = "puzzle3/im7.jpg";
+pdf7="pdf/GUERRADIGITALE_FULL METAL JACKET.pdf";
+
+answ8 = "Il film racconta che nel 2034, grazie allo sviluppo di alcune tecnologie per il controllo remoto della mente, viene lanciato “SLAYER”, un videogioco sparatutto che permette ai player di telecomandare con un dispositivo le azioni di alcuni condannati a morte che si sfidano l’un l’altro in veri e propri campi di guerra, nella speranza di sopravvivere per 30 round, dopo i quali possono essere liberati. A quale idea di soldato del futuro fa pensare questo scenario?";
+q8 = ["A quella di un soldato deresponsabilizzato che vive la guerra come un gioco ", "A quella di un soldato debole che non è nulla senza la tecnologia", "Alla scomparsa del soldato che difende la patria", "A quella di un soldato che cerca di mantenere la sua umanità"];
+c8 = "A quella di un soldato deresponsabilizzato che vive la guerra come un gioco ";
+im8 = "puzzle3/im8.jpg";
+pdf8="pdf/GUERRADIGITALE_GAMER.pdf";
+
+answ9 = "Il film è ispirato alle reali esperienze vissute dal regista nel 1967-1968. Intento dell’autore è:";
+q9 = ["Mostrare cosa hanno provato i soldati in Vietnam in una situazione di guerra e paura.", "Mostrare il modo di vivere dei vietnamiti", "Mostrare la cultura in Vietnam", "Divertire lo spettatore"];
+c9 = "Mostrare cosa hanno provato i soldati in Vietnam in una situazione di guerra e paura.";
+im9 = "puzzle3/im9.jpg";
+pdf9="pdf/GUERRADIGITALE_PLATOON.pdf";
+
+answ10 = "Le tesi sviluppate fanno riferimento al patriottismo in contrasto con la furbizia dell'utilizzare questo valore per scopi non benefici. Nel film il soldato: ";
+q10 = ["Combatte per la patria, prima sul campo di battaglia, poi per la protezione del Presidente ", "È del tutto immerso nella Grande Guerra", "Deplora le armi", "È molto furbo"];
+c10 = "Combatte per la patria, prima sul campo di battaglia, poi per la protezione del Presidente ";
+im10 = "puzzle3/im10.jpg";
+pdf10="pdf/GUERRADIGITALE_SHOOTER.pdf";
+
+answ11 = "Il drone si allontana dagli scenari bellici per un utilizzo a favore dell’uomo. Ma il riferimento non dichiarato resta: guerra invisibile e sorveglianza. Quale rapporto con le armi emerge? ";
+q11 = ["Abilità nel gestire e modificare le rotte e destinazioni tramite determinate applicazioni", "Presenza fisica del soldato", "Aiuto umanitario di un’arma", "Nessun rapporto"];
+c11 = "Abilità nel gestire e modificare le rotte e destinazioni tramite determinate applicazioni";
+im11 = "puzzle3/im11.jpg";
+pdf11="pdf/GUERRADIGITALE_DRONE.pdf";
+
+answ12 = "Il pilota fisicamente impugna il controller del drone e non necessariamente ha esperienza sul campo: questo nuovo modo di fare la guerra può sembrare un videogioco, ma: ";
+q12 = ["Lo stress è più pesante poiché il contatto visivo è molto più ravvicinato  di quanto possa esserlo per un pilota di aereo.", "La situazione è più leggera", "Non si riflette sul peso dei crimini", "Il soldato non ne ha contezza."];
+c12 = "Lo stress è più pesante poiché il contatto visivo è molto più ravvicinato  di quanto possa esserlo per un pilota di aereo.";
+im12 = "puzzle3/im12.jpg";
+pdf12="pdf/GUERRADIGITALE__PilotiDroni.pdf";
+
+AllAnsw = [answ1, answ2,answ3,answ4,answ5,answ6,answ7,answ8,answ9,answ10,answ11,answ12];
+AllIm = [im1, im2,im3,im4,im5,im6,im7,im8,im9,im10,im11,im12];
+AllQ = [q1, q2, q3,q4,q5,q6,q7,q8,q9,q10,q11,q12];
+AllC = [c1, c2, c3,c4,c5,c6,c7,c8,c9,c10,c11,c12];
+AllPdf=[pdf1,pdf2,pdf3,pdf4,pdf5,pdf6,pdf7,pdf8,pdf9,pdf10,pdf11,pdf12];
+
+// Evt--> Events
+var CorrQ = 0;
+var Piece = document.getElementsByClassName('movil'); //Add on img
+
+
+//Generare AnswerDiv
+function Genera() {
+	for (var i = 0; i < Piece.length; i++) {
+		Piece[i].setAttribute("width", 200); //The width of img
+		Piece[i].setAttribute("height", 114.5); //The width of img
+		Piece[i].setAttribute("x", Math.floor((Math.random() * 10) + 1));
+		Piece[i].setAttribute("y", Math.floor((Math.random() * 600) + 1));
+		Piece[i].setAttribute("onmousedown", "SelectElement(evt)");
+	}
+
+	Div = document.getElementById("AnswerDiv");
+	for (var i = 0; i < 12; i++) {
+		var DivAnsw = document.createElement("div");
+		DivAnsw.className = "Hide";
+		DivAnsw.id = "AnswerDivid" + (i);
+
+		var Form = document.createElement("form"); // Create the FORM WITH RADIO, BUTTON QUESTION AND IMG
+
+		var Risp = AllQ[i];
+		shuffle(Risp);
+
+		var Img = document.createElement("img");
+		Img.src = AllIm[i];
+		Form.appendChild(Img);
+		var mybr = document.createElement('br');
+		Form.appendChild(mybr);
+		var PDF=document.createElement("a");
+		PDF.href=AllPdf[i];
+		PDF.innerHTML="Vedi scheda di analisi";
+		Form.appendChild(PDF);
+		mybr =document.createElement('br');
+		Form.appendChild(mybr);
+		Form.appendChild(document.createElement("h1").appendChild(document.createTextNode(AllAnsw[i])));//Add ANSWER
+
+
+		var List =
+			document.createElement("ul");
+		for (var x = 0; x < 4; x++) {
+			var Lbl = document.createElement("li");
+			var Radio = document.createElement("INPUT");
+			Radio.setAttribute("type", "radio");
+			Lbl.innerHTML = Risp[x];
+			Radio.value = Risp[x];
+			Radio.id = "Answ" + (x + 1) + (i + 1); //Answ NDom NQuadro
+			Radio.name = "Answ" + (i + 1);
+			Lbl.appendChild(Radio);
+			List.appendChild(Lbl);
+			Form.appendChild(List);
+
+		}
+		var Btn = document.createElement("button");
+		//Btn.setAttribute("type", "button");
+		Btn.innerHTML = "Conferma risposta";
+		Btn.id = "btn" + (i);
+		Btn.addEventListener("click", function () { CheckAnswer(this); });
+
+		DivAnsw.appendChild(Form);
+		DivAnsw.appendChild(Btn);
+		Div.appendChild(DivAnsw);
+
+	}
+
+}
+function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+var elementSelect = 0;
+var currentX = 0;
+var currentY = 0;
+var currentPosX = 0;
+var currentPosY = 0;
+
+function SelectElement(evt) {
+	elementSelect = Reorder(evt); //Select the member
+	currentX = evt.clientX;
+	currentY = evt.clientY;
+	currentPosx = parseFloat(elementSelect.getAttribute("x"));
+	currentPosy = parseFloat(elementSelect.getAttribute("y"));
+	elementSelect.setAttribute("onmousemove", "MoveElement(evt)");
+}
+
+function MoveElement(evt) {
+	var dx = evt.clientX - currentX; //Calculate the pos of the piece
+	var dy = evt.clientY - currentY;
+	currentPosx = currentPosx + dx;
+	currentPosy = currentPosy + dy;
+	elementSelect.setAttribute("x", currentPosx);
+	elementSelect.setAttribute("y", currentPosy);
+	currentX = evt.clientX;        //Save pos of the piece
+	currentY = evt.clientY;
+	elementSelect.setAttribute("onmouseout", "DeselectElement(evt)");
+	elementSelect.setAttribute("onmouseup", "DeselectElement(evt)");
+	Check(); //Check if it is on position
+}
+
+function DeselectElement(evt) {
+	testing(GetFather(evt));
+	if (elementSelect != 0) {
+		elementSelect.removeAttribute("onmousemove");
+		elementSelect.removeAttribute("onmouseout");
+		elementSelect.removeAttribute("onmouseup");
+		elementSelect = 0;
+	}
+}
+
+function Reorder(evt) {
+	var Intorno = document.getElementById('intorno');
+	var padre = evt.target.parentNode; //Select the piece
+	var clone = padre.cloneNode(true);
+	//Bring it UP!
+	var id = padre.getAttribute("id");
+	Intorno.removeChild(document.getElementById(id));
+	Intorno.appendChild(clone);
+	return Intorno.lastChild.firstChild;  //Return the selected piece
+}
+//return the father of the img
+function GetFather(evt) {
+	var padre = evt.target.parentNode; //Select the piece
+	//Bring it UP!
+	var id = padre.getAttribute("id");
+	return id;  //Return the selected piece
+}
+
+//The final value img
+var origX = [200, 400, 600, 200, 400, 600, 200, 400, 600,200, 400, 600];
+var origY = [100, 100, 100, 214, 214, 214,328,328,328, 442, 442, 442];
+
+function Check() { //Check if the piece is in some place where u can pose the img
+	for (var i = 0; i < Piece.length; i++) {
+		if (Math.abs(currentPosx - origX[i]) < 15 && Math.abs(currentPosy - origY[i]) < 15) { //It gives to you 15 px of range
+			elementSelect.setAttribute("x", origX[i]);
+			elementSelect.setAttribute("y", origY[i]);
+		}
+	}
+}
+
+var win = document.getElementById("win");
+
+function testing(id) {
+	var Father=document.getElementById(id);
+	var posx = parseFloat(Father.firstChild.getAttribute("x"));
+	var posy = parseFloat(Father.firstChild.getAttribute("y"));
+	if (origX[id] == posx && origY[id] == posy) {
+		ShowAnswer(id);
+	}
+	else
+	{
+		var AllHide = document.getElementsByClassName("Hide");
+		for (var i = 0; i < AllHide.length; i++) {
+			AllHide[i].style.display = "none";
+		}
+	}
+}
+function CheckAnswer(ID) {
+	var Numb;
+	if(ID.id.length==4)
+	{
+	Numb = ID.id.substr(ID.id.length - 1)
+	}
+	else
+	{
+	Numb = ID.id.substr(ID.id.length - 2)
+	}
+    var div = document.getElementById("AnswerDivid" + Numb);
+    var Radio = div.getElementsByTagName("INPUT");
+    //alert(Radio.length);
+    var ok = false;
+    for (var i = 0; i < Radio.length && !ok; i++) {
+        //alert(Radio[i].id+" "+Radio[i].checked);
+        if (Radio[i].checked) {
+            //alert(AllC[(ID.id.substr(ID.id.length - 1)) - 1]);
+            if (Radio[i].value == AllC[Numb]) {
+                ok = true;
+            }
+            //alert("VINTO");
+        }
+        //alert(Radio.length);
+    }
+    if (ok) {
+		alert("Risposta Esatta!");
+		div.style.display = "none";
+		CorrQ=CorrQ+1;
+		var Father=document.getElementById(Numb);
+		//alert(Numb);
+		Father.lastChild.setAttribute("onmousedown", "");
+		if(Corrq==12)
+		{
+			// DO SOMETHING BUT I CAN'T REMEMBER WHAT
+			//It should be a response to a page, but i can't find the right image, so GO FUCK YOURSELF...
+			alert("VINTO!!!!!!!! (IMMAGINA UN'IMMAGINE DI VITTORIA NELLA TUA MENTE, FUNZIONE IN VIA DI SVILUPPO");
+		}
+    }
+    else {
+		alert("Risposta sbagliata!");
+		div.style.display = "none";
+		var Father=document.getElementById(Numb);
+		var Piece=Father.lastChild;
+		Piece.setAttribute("x", Math.floor((Math.random() * 500) + 1));
+		Piece.setAttribute("y", Math.floor((Math.random() * 409) + 1));
+    }
+}
+function ShowAnswer(Im) {
+    var AllHide = document.getElementsByClassName("Hide");
+    for (var i = 0; i < AllHide.length; i++) {
+        AllHide[i].style.display = "none";
+    }
+	var div = document.getElementById("AnswerDivid" + Im);
+	div.style.display = "block";
+}
