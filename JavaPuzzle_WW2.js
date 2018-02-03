84901ba5e9da70a6807f65644b1cf4f1d3226110
Francesco Rossetti <francross59@gmail.com>
2018-02-03 14:54:58 +0100

    03022017

---
+++ JavaPuzzle_WW2.js
@@ -0,0 +1,302 @@
+//Instanziare gli elementi per le domande
+//url('../images/divbg.png')
+answ1 = "L’immagine rappresenta una nuova arma, che è in grado di ammarare su specchi d’acqua o su navi portaerei in appoggio alla Marina e di controllare l’area del conflitto dall’alto. Nel periodo che comprende le due guerre mondiali, le tipologie di idrovolanti militari andavano a ricoprire tutte le possibili specializzazioni di un velivolo: caccia, bombardiere, ricognizione e trasporto. Quale rapporto si instaura tra il pilota e lo strumento di guerra?";
+q1 = ["È un soldato che avverte la propria superiorità e la potenza delle proprie armi, anche se rischia la propria vita in duelli aerei", "È un soldato che sperimenta un’arma nuova, di cui sa che non può fidarsi ciecamente", "È un soldato che non si sente affatto diverso da chi combatte una guerra di terra", "È un soldato a cui è stata data un’arma che non richiede particolare coraggio"];
+c1 = "È un soldato che avverte la propria superiorità e la potenza delle proprie armi, anche se rischia la propria vita in duelli aerei";
+im1 = "puzzle2/im1.jpg";
+pdf1="pdf/WWII_IDROVOLANTI.pdf";
+
+answ2 = "Calvino narra la vicenda Pin, un bambino orfano costretto a diventare adulto a causa degli eventi storici in cui si trova a vivere. Un giorno ruba a un soldato tedesco una P-38 e questo evento cambia il corso della sua vita. Pin non è un soldato e non ha ideali che lo spingano a combattere: è il possesso dell’arma che lo trasforma in un combattente. Si può ritrovare anche nelle guerre di oggi un’esperienza come quella di Pin?";
+q2 = ["Sì, il rapporto di Pin con la pistola non differisce da quello che ai nostri giorni hanno i guerriglieri/terroristi/foreign fighters con le loro armi", "Sì, la causa delle guerre di oggi è la facilità con cui si possono ottenere armi, che finiscono in mano ai bambini", "No, nelle guerre di oggi non si potrebbero verificare situazioni analoghe, perché i bambini sono protetti", "No, perché allora si trattava di una guerra asimmetrica, mentre ora le guerre sono tutte simmetriche"];
+c2 = "Sì, il rapporto di Pin con la pistola non differisce da quello che ai nostri giorni hanno i guerriglieri/terroristi/foreign fighters con le loro armi";
+im2 = "puzzle2/im2.jpg";
+pdf2="pdf/WWII_Sentiero dei nidi di ragno.pdf";
+
+answ3 = "Nel film Fury, viene ben rappresentato il rapporto tra soldato e arma a partire dal titolo, che riprende il nome del carro armato Shermann in cui un gruppo di soldati resiste ai furiosi attacchi dei Tedeschi, ormai visini alla disfatta. Il carro armato per questi soldati rappresenta:";
+q3 = ["La loro casa e l’unico mezzo per salvarsi", "Una prigione dentro la quale rischiano di morire", "La causa di tutte le loro sofferenze, da cui non vedono l’ora di uscire", "Uno strumento con cui uccidere quanti più nemici possibile"];
+c3 = "La loro casa e l’unico mezzo per salvarsi";
+im3 = "puzzle2/im3.jpg";
+pdf3="pdf/WWII_FURY.pdf";
+
+answ4 = "In Red Tails i piloti del battaglione Tuskegee Airmen, soprannominato Red Tails, sono tutti uomini di colore, arruolatisi sperando di combattere la discriminazione razziale, che però si manifesta ancora attraverso l’assegnazione di aerei vecchi e meno efficienti. Nonostante questo, le loro azioni sono coraggiose e spesso risolutive. Quale rapporto con le armi, in questo caso gli aerei da caccia, emerge da questa storia?";
+q4 = ["I Red Tails non si fanno fermare dalla minore efficienza dei loro aerei", "I piloti si sentono umiliati e ancora emarginati", "I Red Tails odiano i bianchi perché hanno le armi migliori", "I piloti preferiscono questi vecchi aerei, per dimostrare meglio il proprio eroismo"];
+c4 = "I Red Tails non si fanno fermare dalla minore efficienza dei loro aerei";
+im4 = "puzzle2/im4.jpg";
+pdf4="pdf/WWII_RED TAILS.pdf";
+
+answ5 = "Il film trasmette un indubbio messaggio: la vita di ogni singolo uomo ha un valore inestimabile. Rispetto al soldato, qual è l’obiettivo manifesto?";
+q5 = ["Riportare in patria un soldato","Lasciar perdere i feriti", "Illustrare la tecnologia delle armi", "Nessun obiettivo"];
+c5 = "Riportare in patria un soldato";
+im5 = "puzzle2/im5.jpg";
+pdf5="pdf/WWII_SALVATE IL SOLDATO RYAN.pdf";
+
+answ6 = "La guerra si combatte anche al di fuori del fronte, dietro le linee amiche, nei laboratori. Allora gli avversari sono, non solo i soldati, ma anche: ";
+q6 = ["Gli scienziati per la sfida a livello tecnologico","Gli scrittori e, per primo, Alan Turing", "Gli amici della patria", "Non ci sono altri nemici"];
+c6 = "Gli scienziati per la sfida a livello tecnologico";
+im6 = "puzzle2/im6.jpg";
+pdf6="pdf/WWII_ALAN TURING.pdf";
+
+answ7 = "La fonte descrive la potenza distruttiva fino ad allora sconosciuta, difficile da utilizzare, pena la scomparsa del genere umano. Quale rapporto con l’arma espone?";
+q7 = ["La non consapevolezza della potenza distruttiva dell’arma.","La consapevolezza della potenza distruttiva dell’arma.", "Assoluto controllo della sua forza.", "Nessun rapporto"];
+c7 = "La non consapevolezza della potenza distruttiva dell’arma.";
+im7 = "puzzle2/im7.jpg";
+pdf7="pdf/WWII_LA_BOMBA_ATOMICA.pdf";
+
+answ8 = "Il bombardamento è una prova di forza che agisce su un punto debole: i cittadini inermi e disarmati. Dimostra come:";
+q8 = ["la tecnologia può comportare assenza di emozione nell’uccidere.","la tecnologia non comporta mai assenza di emozione nell’uccidere.", "L’azione sia utile a vincere la guerra.", "Non dimostra nulla."];
+c8 = "la tecnologia può comportare assenza di emozione nell’uccidere.";
+im8 = "puzzle2/im8.jpg";
+pdf8="pdf/WWII Bombardamento di Dresda.pdf";
+
+answ9 = "Il bombardamento nucleare su Hiroshima e Nagasaki fu, all’interno di una guerra simmetrica, un episodio di guerra asimmetrica, dato che tre aviatori, con una sola bomba, colpirono circa 350.000, persone, per lo più civili. L’uso dell’atomica trasformò il rapporto tra aviatore che bombarda e vittime, perché:";
+q9 = ["gli effetti di questo micidiale ordigno non si limitano ad uccidere chi viene colpito nell’immediato dal bombardamento, ma si manifestano per generazioni, amplificando il potere di quell’atto di guerra e del soldato che lo esegue","una sola bomba poteva uccidere molti più nemici, quindi bastava impegnare pochi piloti fidati e non rischiare che qualche aviatore avesse crisi di coscienza", "le atomiche potevano essere sganciate da altitudini maggiori, quindi il pilota non aveva nessuna possibilità di vedere le sue vittime", "data la violenza dell’esplosione, il pilota poteva essere colpito a sua volta e questo lo rendeva più sensibile alla sofferenza delle vittime"];
+c9 = "gli effetti di questo micidiale ordigno non si limitano ad uccidere chi viene colpito nell’immediato dal bombardamento, ma si manifestano per generazioni, amplificando il potere di quell’atto di guerra e del soldato che lo esegue";
+im9 = "puzzle2/im9.jpg";
+pdf9="pdf/WWII_HIROSHIMA.pdf";
+
+answ10 = "La fonte chiarisce l’evoluzione della tecnologia che introduce l’uso dell’informatica in campo bellico. Quale rapporto con le armi emerge?";
+q10 = ["Chi utilizza Enigma non ha rapporti con le armi, ma deve essere abile nel dare informazioni a chi le utilizza","Chi utilizza Enigma ha rapporti con le armi al pari degli altri soldati", "Chi utilizza Enigma ha rapporti solo con alcune armi", "Non emerge nulla"];
+c10 = "Chi utilizza Enigma non ha rapporti con le armi, ma deve essere abile nel dare informazioni a chi le utilizza";
+im10 = "puzzle2/im10.jpg";
+pdf10="pdf/WWII_IMMAGINE_DELLA_MACCHINA_ENIGMA.pdf";
+
+answ11 = "La guerra è combattuta anche da due analisti, distanti dal campo di battaglia. Fra gli obiettivi dell’autore c’è anche:";
+q11 = ["Informare sulla scienza e alto contenuto tecnologico che vengono spese a servizio della guerra","Informare sulla scienza e alto contenuto tecnologico che vengono lasciate lontane dalla guerra", "Informare sui costumi dell’epoca", "Non c’è alcun obiettivo"];
+c11 = "Informare sulla scienza e alto contenuto tecnologico che vengono spese a servizio della guerra";
+im11 = "puzzle2/im11.jpg";
+pdf11="pdf/WWII  Imitation Game e Enigma_tmp.pdf";
+
+answ12 = "La macchina Enigma rappresenta: ";
+q12 = ["Una nuova arma di supporto alle azioni belliche","Una vecchia arma di supporto alle azioni belliche", "Un’arma che non funziona", "Non rappresenta nulla"];
+c12 = "Una nuova arma di supporto alle azioni belliche";
+im12 = "puzzle2/im12.jpg";
+pdf12="pdf/WWII Enigma Bersia Canale.pdf";
+
+AllAnsw = [answ1, answ2,answ3,answ4,answ5,answ6,answ7,answ8,answ9,answ10,answ11,answ12];
+AllIm = [im1, im2,im3,im4,im5,im6,im7,im8,im9,im10,im11,im12];
+AllQ = [q1, q2, q3,q4,q5,q6,q7,q8,q9,q10,q11,q12];
+AllC = [c1, c2, c3,c4,c5,c6,c7,c8,c9,c10,c11,c12];
+AllPdf=[pdf1,pdf2,pdf3,pdf4,pdf5,pdf6,pdf7,pdf8,pdf9,pdf10,pdf11,pdf12];
+
+// Evt--> Events
+var CorrQ = 0;
+var Piece = document.getElementsByClassName('movil'); //Add on img
+
+
+//Generare AnswerDiv
+function Genera() {
+	for (var i = 0; i < Piece.length; i++) {
+		Piece[i].setAttribute("width", 200); //The width of img
+		Piece[i].setAttribute("height", 114.5); //The width of img
+		Piece[i].setAttribute("x", Math.floor((Math.random() * 10) + 1));
+		Piece[i].setAttribute("y", Math.floor((Math.random() * 600) + 1));
+		Piece[i].setAttribute("onmousedown", "SelectElement(evt)");
+	}
+
+	Div = document.getElementById("AnswerDiv");
+	for (var i = 0; i < 12; i++) {
+		var DivAnsw = document.createElement("div");
+		DivAnsw.className = "Hide";
+		DivAnsw.id = "AnswerDivid" + (i);
+
+		var Form = document.createElement("form"); // Create the FORM WITH RADIO, BUTTON QUESTION AND IMG
+
+		var Risp = AllQ[i];
+		shuffle(Risp);
+
+		var Img = document.createElement("img");
+		Img.src = AllIm[i];
+		Form.appendChild(Img);
+		var mybr = document.createElement('br');
+		Form.appendChild(mybr);
+		var PDF=document.createElement("a");
+		PDF.href=AllPdf[i];
+		PDF.innerHTML="Vedi scheda di analisi";
+		Form.appendChild(PDF);
+		mybr =document.createElement('br');
+		Form.appendChild(mybr);
+		Form.appendChild(document.createElement("h1").appendChild(document.createTextNode(AllAnsw[i])));//Add ANSWER
+
+
+		var List =
+			document.createElement("ul");
+		for (var x = 0; x < 4; x++) {
+			var Lbl = document.createElement("li");
+			var Radio = document.createElement("INPUT");
+			Radio.setAttribute("type", "radio");
+			Lbl.innerHTML = Risp[x];
+			Radio.value = Risp[x];
+			Radio.id = "Answ" + (x + 1) + (i + 1); //Answ NDom NQuadro
+			Radio.name = "Answ" + (i + 1);
+			Lbl.appendChild(Radio);
+			List.appendChild(Lbl);
+			Form.appendChild(List);
+
+		}
+		var Btn = document.createElement("button");
+		//Btn.setAttribute("type", "button");
+		Btn.innerHTML = "Conferma risposta";
+		Btn.id = "btn" + (i);
+		Btn.addEventListener("click", function () { CheckAnswer(this); });
+
+		DivAnsw.appendChild(Form);
+		DivAnsw.appendChild(Btn);
+		Div.appendChild(DivAnsw);
+
+	}
+
+}
+function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+var elementSelect = 0;
+var currentX = 0;
+var currentY = 0;
+var currentPosX = 0;
+var currentPosY = 0;
+
+function SelectElement(evt) {
+	elementSelect = Reorder(evt); //Select the member
+	currentX = evt.clientX;
+	currentY = evt.clientY;
+	currentPosx = parseFloat(elementSelect.getAttribute("x"));
+	currentPosy = parseFloat(elementSelect.getAttribute("y"));
+	elementSelect.setAttribute("onmousemove", "MoveElement(evt)");
+}
+
+function MoveElement(evt) {
+	var dx = evt.clientX - currentX; //Calculate the pos of the piece
+	var dy = evt.clientY - currentY;
+	currentPosx = currentPosx + dx;
+	currentPosy = currentPosy + dy;
+	elementSelect.setAttribute("x", currentPosx);
+	elementSelect.setAttribute("y", currentPosy);
+	currentX = evt.clientX;        //Save pos of the piece
+	currentY = evt.clientY;
+	elementSelect.setAttribute("onmouseout", "DeselectElement(evt)");
+	elementSelect.setAttribute("onmouseup", "DeselectElement(evt)");
+	Check(); //Check if it is on position
+}
+
+function DeselectElement(evt) {
+	testing(GetFather(evt));
+	if (elementSelect != 0) {
+		elementSelect.removeAttribute("onmousemove");
+		elementSelect.removeAttribute("onmouseout");
+		elementSelect.removeAttribute("onmouseup");
+		elementSelect = 0;
+	}
+}
+
+function Reorder(evt) {
+	var Intorno = document.getElementById('intorno');
+	var padre = evt.target.parentNode; //Select the piece
+	var clone = padre.cloneNode(true);
+	//Bring it UP!
+	var id = padre.getAttribute("id");
+	Intorno.removeChild(document.getElementById(id));
+	Intorno.appendChild(clone);
+	return Intorno.lastChild.firstChild;  //Return the selected piece
+}
+//return the father of the img
+function GetFather(evt) {
+	var padre = evt.target.parentNode; //Select the piece
+	//Bring it UP!
+	var id = padre.getAttribute("id");
+	return id;  //Return the selected piece
+}
+
+//The final value img
+var origX = [200, 400, 600, 200, 400, 600, 200, 400, 600,200, 400, 600];
+var origY = [100, 100, 100, 214, 214, 214,328,328,328, 442, 442, 442];
+
+function Check() { //Check if the piece is in some place where u can pose the img
+	for (var i = 0; i < Piece.length; i++) {
+		if (Math.abs(currentPosx - origX[i]) < 15 && Math.abs(currentPosy - origY[i]) < 15) { //It gives to you 15 px of range
+			elementSelect.setAttribute("x", origX[i]);
+			elementSelect.setAttribute("y", origY[i]);
+		}
+	}
+}
+
+var win = document.getElementById("win");
+
+function testing(id) {
+	var Father=document.getElementById(id);
+	var posx = parseFloat(Father.firstChild.getAttribute("x"));
+	var posy = parseFloat(Father.firstChild.getAttribute("y"));
+	if (origX[id] == posx && origY[id] == posy) {
+		ShowAnswer(id);
+	}
+	else
+	{
+		var AllHide = document.getElementsByClassName("Hide");
+		for (var i = 0; i < AllHide.length; i++) {
+			AllHide[i].style.display = "none";
+		}
+	}
+}
+function CheckAnswer(ID) {
+    var Numb = ID.id.substr(ID.id.length - 1)
+    var div = document.getElementById("AnswerDivid" + Numb);
+    var Radio = div.getElementsByTagName("INPUT");
+    //alert(Radio.length);
+    var ok = false;
+    for (var i = 0; i < Radio.length && !ok; i++) {
+        //alert(Radio[i].id+" "+Radio[i].checked);
+        if (Radio[i].checked) {
+            //alert(AllC[(ID.id.substr(ID.id.length - 1)) - 1]);
+            if (Radio[i].value == AllC[Numb]) {
+                ok = true;
+            }
+            //alert("VINTO");
+        }
+        //alert(Radio.length);
+    }
+    if (ok) {
+		alert("Risposta Esatta!");
+		div.style.display = "none";
+		CorrQ=CorrQ+1;
+		var Father=document.getElementById(Numb);
+		//alert(Numb);
+		Father.lastChild.setAttribute("onmousedown", "");
+		if(Corrq==12)
+		{
+			// DO SOMETHING BUT I CAN'T REMEMBER WHAT
+			//It should be a response to a page, but i can't find the right image, so GO FUCK YOURSELF...
+			alert("VINTO!!!!!!!! (IMMAGINA UN'IMMAGINE DI VITTORIA NELLA TUA MENTE, FUNZIONE IN VIA DI SVILUPPO");
+		}
+    }
+    else {
+		alert("Risposta sbagliata!");
+		div.style.display = "none";
+		var Father=document.getElementById(Numb);
+		var Piece=Father.lastChild;
+		Piece.setAttribute("x", Math.floor((Math.random() * 500) + 1));
+		Piece.setAttribute("y", Math.floor((Math.random() * 409) + 1));
+    }
+}
+function ShowAnswer(Im) {
+    var AllHide = document.getElementsByClassName("Hide");
+    for (var i = 0; i < AllHide.length; i++) {
+        AllHide[i].style.display = "none";
+    }
+	var div = document.getElementById("AnswerDivid" + Im);
+	div.style.display = "block";
+}
